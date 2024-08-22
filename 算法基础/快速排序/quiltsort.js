8b19891442dcd58ee362195b3cd6aac7713fae0c
孟南 <76862521+Mengnan578@users.noreply.github.com>
2024-08-23 00:34:50 +0800

    第一次提交

---
+++ 算法基础/快速排序/quiltsort.js
@@ -0,0 +1,22 @@
+/**
+ * 快速排序
+ * 1. 确定分界点
+ * 2. 左右两边排序
+ * 3. 递归左右两边
+ */
+const rl = require("readline")
+const readLine = rl.createInterface({
+    input: process.stdin,
+    output:process.stdin
+});
+
+let anw = "";
+
+readLine.on("line",arr => {
+    // 读取数据，调用函数    
+})
+
+readLine.on("close",() => {
+console.log()  
+}) 
+
